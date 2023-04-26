4acfa6a7561a69ed7a6427c8da30ce5d2b6605f1
jeronimo_nuñez <jeronimonunez96@gmail.com>
2023-04-25 23:15:29 -0300

    Agregue sections y footer

---
+++ main.js
@@ -0,0 +1,12 @@
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+const slider = document.querySelector('.slider')
+
+prev.addEventListener('click', () => {
+    slider.scrollLeft -= 300
+})
+
+prev.addEventListener('click', () => {
+    slider.scrollLeft += 300
+})
+
